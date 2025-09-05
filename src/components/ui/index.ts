eefe2eaa35c91d5935ed241880b336f9cb454ec9
Sagar <sagarsahu5976@gmail.com>
2025-09-05 14:45:19 +0530

    created index.ts bcs of module resolution difference

---
+++ src/components/ui/index.ts
@@ -0,0 +1,7 @@
+export { Button } from './Button'
+export { Card, GlareCard } from './Card'
+export { default as GlareHover } from './GlareHover'
+export { Input } from './Input'
+export { default as WaveBackground } from './WaveBackground'
+
+
