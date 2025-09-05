df7bc5eaddd13d1245bcdc1c69ccc2af47919e3a
Sagar <sagarsahu5976@gmail.com>
2025-09-05 15:36:17 +0530

    idk

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -1,7 +1,5 @@
 export { Button } from './Button.tsx'
 export { Card, GlareCard } from './Card.tsx'
 export { default as GlareHover } from './GlareHover.tsx'
-export { Input } from './Input.tsx'
+export { default as Input } from './Input.tsx' // Changed this line
 export { default as WaveBackground } from './WaveBackground.tsx'
-
-
