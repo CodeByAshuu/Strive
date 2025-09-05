8d7da37e88e8723ca7e466d1099614ceb88c5cc2
Sagar <sagarsahu5976@gmail.com>
2025-09-05 15:17:09 +0530

    added extension in index.ts

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -1,7 +1,7 @@
-export { Button } from './Button'
-export { Card, GlareCard } from './Card'
-export { default as GlareHover } from './GlareHover'
-export { Input } from './Input'
-export { default as WaveBackground } from './WaveBackground'
+export { Button } from './Button.tsx'
+export { Card, GlareCard } from './Card.tsx'
+export { default as GlareHover } from './GlareHover.tsx'
+export { Input } from './Input.tsx'
+export { default as WaveBackground } from './WaveBackground.tsx'
 
 
